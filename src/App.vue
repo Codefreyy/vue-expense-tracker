cc3457f8d34bf21bd0c86dafda75ec4c204c8404
godfrey <joyyujiepeng@gmail.com>
2023-12-02 20:28:20 +0000

    feat: show Income and Expense figure dynamically

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <Header />
   <div class="container">
     <Balance :totalBalance="totalBalance" />
-    <IncomeExpenses />
+    <IncomeExpenses :incomes="incomes" :expenses="expenses" />
     <transaction-list :transactions="transactions" />
     <add-transaction />
   </div>
@@ -20,11 +20,33 @@ import { ref, computed } from "vue"
 const transactions = ref([
   { id: 1, text: "Flower", amount: -19.99 },
   { id: 2, text: "Salary", amount: 299.97 },
+  { id: 3, text: "Movie", amount: -10.0 },
 ])
 
+// get balance
 const totalBalance = computed(() => {
   return transactions.value.reduce((acc, transaction) => {
     return acc + transaction.amount
+  }, 0)
+})
+
+// get income
+const incomes = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    if (transaction.amount > 0) {
+      return acc + transaction.amount
+    }
+    return acc
+  }, 0)
+})
+
+// get expenses
+const expenses = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    if (transaction.amount < 0) {
+      return acc + transaction.amount * -1
+    }
+    return acc
   }, 0)
 })
 </script>
